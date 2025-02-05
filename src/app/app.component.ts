a58b11c068deb22736bb2fb4c4ea0a1acc20ff28
Alaa-Awad0 <loly50987@gmail.com>
2025-02-05 06:39:35 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MealsComponent } from "./meals/meals.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MealsComponent],
+  imports: [MealsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
